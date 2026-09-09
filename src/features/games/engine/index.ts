663e899f832a65be9fc6b7b092eab5080cd3c69c
NaiBossZer <apiwat.suw@mahidol.ac.th>
2026-09-10 05:33:47 +0700

    refactor(game): expose game controllers

--- src/features/games/engine/index.ts
+++ src/features/games/engine/index.ts
@@ -1,2 +1,4 @@
 export * from "@/features/bingo/engine/LacBingoEngine";
+export * from "@/features/bingo/engine/useLacBingoEngine";
 export * from "@/features/sobprab/engine/SobprabLacLabEngine";
+export * from "@/features/sobprab/engine/useSobprabLacLabEngine";
